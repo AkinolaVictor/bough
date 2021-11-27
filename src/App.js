7e5c13257b49acf83c54f27c2470d6598ec5f24f
akinolavictor <akinolavictor26@gmail.com>
2021-11-27 09:55:09 +0100

    initial commit of all files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+// import App2 from './App2';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        {/* <App2 /> */}
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <br /> 
+        <br /> 
+        <br /> 
+        <br /> 
+        <br /> 
+      </header>
+    </div>
+  );
+}
+
+export default App;
